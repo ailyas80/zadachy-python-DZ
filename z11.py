c8d12d51051470953703a0523af7b535845fd55b
ILYAS <ailyas80@mail.ru>
2022-03-16 21:42:01 +0400

    z11

---
+++ z11.py
@@ -0,0 +1,14 @@
+# Сформировать список из  N членов последовательности.
+# Для N = 5: 1, -3, 9, -27, 81 и т.д.
+Nstr = input("введите N:")
+tmp = 1
+list1 = []
+list1.append(tmp)
+if Nstr == '':
+    print('некорректный ввод')
+else:
+    N = int(Nstr)
+    for i in range(N-1):
+        tmp *= -3
+        list1.append(tmp)
+print(list1)
