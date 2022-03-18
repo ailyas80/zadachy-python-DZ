830c121f8e18920e4c01fbbd69ac5b4c0783c797
ILYAS <ailyas80@mail.ru>
2022-03-19 00:19:17 +0400

    z14/4

--- z14.py
+++ z14.py
@@ -4,17 +4,22 @@
 #     N = N * 10   # N = round(N * 10, 3)
 # N = N // 10
 # print(N)
+# str_lst = ['1', '2', '3']
+# int_lst = [int(x) for x in str_lst]
+# print (int_lst)
 
 N = str(input("Введите число: "))
 sum = 0
 list = []
 for i in N:
     if i != '.':
-     list.append(i)
+        list.append(i)
 print(list)
-# for i in list:
-#     sum += list[i]
-# print(sum)
+int_lst = [int(x) for x in list]
+print(int_lst[1])
+for i in int_lst:
+    sum = sum + i
+print(sum)
 
 
 # def sum_of_digits(num):
