ceecaf3656f9738badf6b4d754cbb795910ae665
ILYAS <ailyas80@mail.ru>
2022-03-18 23:59:22 +0400

    z14/3

--- z14.py
+++ z14.py
@@ -1,15 +1,27 @@
 # Подсчитать сумму цифр в вещественном числе.
-N = float(input("введите число: "))
-while N % 10 > 0:
-    N = N * 10    # N = round(N * 10, 3)
-N = N // 10
-print(N)
+# N = float(input("введите число: "))
+# while N % 10 != 0:
+#     N = N * 10   # N = round(N * 10, 3)
+# N = N // 10
+# print(N)
 
-def sum_of_digits(num):
-    sum = 0
-    while num > 0:
-        sum += num % 10
-        num //= 10
-    return sum
+N = str(input("Введите число: "))
+sum = 0
+list = []
+for i in N:
+    if i != '.':
+     list.append(i)
+print(list)
+# for i in list:
+#     sum += list[i]
+# print(sum)
 
-print(sum_of_digits(N))
+
+# def sum_of_digits(num):
+#     sum = 0
+#     while num > 0:
+#         sum += num % 10
+#         num //= 10
+#     return sum
+
+# print(sum_of_digits(N))
