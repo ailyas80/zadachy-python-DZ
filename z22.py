37f2e2ef1aeeea0c7448142ad659b00a6faab2b7
ILYAS <ailyas80@mail.ru>
2022-03-19 17:43:34 +0400

    z22

---
+++ z22.py
@@ -0,0 +1,7 @@
+# Найти сумму чисел списка стоящих на нечетной позиции
+list = [1, 2, 3, 4, 5, 6, 7]
+sum = 0
+for i in range(len(list)):
+    if list[i]% 2 !=0:
+     sum = sum + list[i]
+print(sum)
