d72545d9ed66b32d12509f3ee21fcc96f751b4d6
ILYAS <ailyas80@mail.ru>
2022-03-19 14:51:03 +0400

    z15/88

--- z15.py
+++ z15.py
@@ -1,10 +1,11 @@
 # Написать программу получающую набор произведений чисел от 1 до N.
 # Пример: пусть N = 4, тогда
 # [ 1, 2, 6, 24 ]
-N = input("Введите число: ")
+N = int(input("Введите число: "))
 list = []
 product_of_numbers = 1
-for i in N:
-    list.append(product_of_numbers * i)
+for i in range(1, N+1):
+    product_of_numbers = product_of_numbers * i
+    list.append(product_of_numbers)
 print(list)
 
