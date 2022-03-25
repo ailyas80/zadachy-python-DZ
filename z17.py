d02192e7ad6d6124da1dbd66e66b913e9d6d6d82
ILYAS <ailyas80@mail.ru>
2022-03-26 00:21:12 +0400

    z17-3

--- z17.py
+++ z17.py
@@ -2,14 +2,38 @@
 # Найти произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число
 
+N = int(input('N = '))
 list = []
-with open('D:/z17.txt', 'r') as file:
- line = file.readline()
- while line:
-     print(line, end="")
-     line = file.readline()
-     list.append(line)
+list2 = []
+y = 0
+x = N * (-1)
+while x <= N:
+    list.append(x)
+    a = x * y
+    x += 1
+    list2.append(a)
+    y += 1  
+    
+    
 print(list)
+print(list2)
+
+    
+
+
+
+
+
+
+
+# list = []
+# with open('D:/z17.txt', 'r') as file:
+#  line = file.readline()
+#  while line:
+#      print(line, end="")
+#      line = file.readline()
+#      list.append(line)
+# print(list)
 
 
 
