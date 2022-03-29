fc7c9f85ebbe959252bc7e762e53cef02fa16870
ILYAS <ailyas80@mail.ru>
2022-03-29 09:58:32 +0400

    z12- new version-2

--- z12.py
+++ z12.py
@@ -1,5 +1,5 @@
 # Для натурального n создать словарь индекс-значение,
-# состоящий из элементов последовательности 3n + 1. 
+# состоящий из элементов последовательности 3n + 1.
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 # N = int(input("введите N:"))
 # index = []
@@ -13,8 +13,13 @@
 #     if i < N-1:
 #      print(', ', end="")
 # print('}')
+
+# n = int(input("Введите n: "))
+# myDictionary = {}
+# for i in range(1, n + 1):
+#     myDictionary[i]= 3*i + 1
+# print(f"для n = {n}: {myDictionary}")
+
 n = int(input("Введите n: "))
-myDictionary = {}
-for i in range(1, n + 1):
-    myDictionary[i]= 3*i + 1
+myDictionary = {i: 3*i + 1 for i in range(1, n+1)}
 print(f"для n = {n}: {myDictionary}")
