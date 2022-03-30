8e5c877a53c9ca1941ab7f4b2ee8ad02b1b21474
ILYAS <ailyas80@mail.ru>
2022-03-30 22:36:58 +0400

    z23

--- z23.py
+++ z23.py
@@ -1,3 +1,18 @@
 # Найти произведение пар чисел в списке. Парой считаем первый и последний элемент,
 #  второй и предпоследний и т.д. 
 #  Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; [2, 3, 5, 6] => [12, 15] 
+def MultPair(listMultPair):
+    listAdd = []
+    if (len(listMultPair) % 2 == 0):
+        for i in range(0, len(listMultPair)//2):
+            listAdd.append(listMultPair[i] * listMultPair[-1-i])
+    else:
+        for i in range(0, (len(listMultPair)+1)//2):
+            listAdd.append(listMultPair[i] * listMultPair[-1-i])
+    return listAdd
+
+
+list1 = [2, 3, 4, 5, 6]
+list2 = [2, 3, 4, 5, 6, 7]
+print(MultPair(list1))
+print(MultPair(list2))
