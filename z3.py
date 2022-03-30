98f0c7a252b2a5080442a788d9404b056cb57df3
ILYAS <ailyas80@mail.ru>
2022-03-30 21:32:48 +0400

    z3-new

--- z3.py
+++ z3.py
@@ -1,8 +1,16 @@
 # Вывести на экран числа от -N до N
 N = int(input('N = '))
 list = []
-x = N * (-1)
+x = -N
 while x <= N:
     list.append(x)
     x += 1
 print(list)
+
+
+
+# N = int(input('N = '))
+# count = -N
+# while count <= N:
+#     print('{0}'.format(count))
+#     count += 1
