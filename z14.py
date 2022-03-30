2ea11d9fdcbf1c68e42899112cdff34d11f87ff3
ILYAS <ailyas80@mail.ru>
2022-03-30 21:50:35 +0400

    z14-new

--- z14.py
+++ z14.py
@@ -6,16 +6,16 @@
 # print(N)
 
 
-N = str(input("Введите число: "))
-sum = 0
-list = []
-for i in N:
-    if i != '.':
-        list.append(i)
-int_lst = [int(j) for j in list]
-for i in int_lst:
-    sum += i
-print(sum)
+# N = str(input("Введите число: "))
+# sum = 0
+# list = []
+# for i in N:
+#     if i != '.':
+#         list.append(i)
+# int_lst = [int(j) for j in list]
+# for i in int_lst:
+#     sum += i
+# print(sum)
 
 
 # def sum_of_digits(num):
@@ -26,3 +26,11 @@ print(sum)
 #     return sum
 
 # print(sum_of_digits(N))
+
+N = str(input("Введите число: "))
+sum = 0
+number = list(N)
+number.remove('.')
+for i in number:
+    sum += int(i)
+print(sum)
