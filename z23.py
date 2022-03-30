98227dd2d67a4c2059b9463f9dcb760d0a897ba6
ILYAS <ailyas80@mail.ru>
2022-03-30 23:16:09 +0400

    z23-2

--- z23.py
+++ z23.py
@@ -1,13 +1,15 @@
 # Найти произведение пар чисел в списке. Парой считаем первый и последний элемент,
-#  второй и предпоследний и т.д. 
-#  Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; [2, 3, 5, 6] => [12, 15] 
+#  второй и предпоследний и т.д.
+#  Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; [2, 3, 5, 6] => [12, 15]
 def MultPair(listMultPair):
     listAdd = []
     if (len(listMultPair) % 2 == 0):
         for i in range(0, len(listMultPair)//2):
+            print(len(listMultPair))
             listAdd.append(listMultPair[i] * listMultPair[-1-i])
     else:
         for i in range(0, (len(listMultPair)+1)//2):
+            print(len(listMultPair))
             listAdd.append(listMultPair[i] * listMultPair[-1-i])
     return listAdd
 
@@ -16,3 +18,16 @@ list1 = [2, 3, 4, 5, 6]
 list2 = [2, 3, 4, 5, 6, 7]
 print(MultPair(list1))
 print(MultPair(list2))
+
+# def MultPair(listMultPair):
+#     listAdd = []
+#     for i in range(0, len(listMultPair)+1//2):
+#         print(len(listMultPair))
+#         listAdd.append(listMultPair[i] * listMultPair[-1-i])
+#     return listAdd
+
+
+# list1 = [2, 3, 4, 5, 6]
+# list2 = [2, 3, 4, 5, 6, 7]
+# print(MultPair(list1))
+# print(MultPair(list2))
