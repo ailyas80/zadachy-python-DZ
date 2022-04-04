852b11537daecaecbc8fb52acc7f85e9e7ef382f
ILYAS <ailyas80@mail.ru>
2022-04-04 21:39:37 +0400

    z43

---
+++ z43.py
@@ -0,0 +1,10 @@
+# 43. Дана последовательность чисел. Получить список уникальных элементов 
+# заданной последовательности. Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10]
+list1 = [1, 2, 3, 5, 1, 5, 3, 10]
+list_unicum = []
+for i in list1:
+    if i not in list_unicum:
+        list_unicum.append(i)
+    else:
+        list_unicum.remove(i)
+print(list_unicum)
