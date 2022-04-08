cab8ac32650edd31bf15a1dbc61f4cae1d570d14
ILYAS <ailyas80@mail.ru>
2022-04-08 20:08:22 +0400

    z26-3

--- z26.py
+++ z26.py
@@ -18,38 +18,38 @@
 # print(fibonacci(n))
 
 
-def fibonachi(n):
-    fib1 = 0
-    fib2 = 1
-    fib = [fib1, fib2]
-    i = 0
-    while i < n - 2:
-     fib_sum = fib1 + fib2
-     fib1 = fib2
-     fib2 = fib_sum
-     fib.append(fib_sum)
-     i += 1
-    return fib
+# def fibonachi(n):
+#     fib1 = 0
+#     fib2 = 1
+#     fib = [fib1, fib2]
+#     i = 0
+#     while i < n - 2:
+#      fib_sum = fib1 + fib2
+#      fib1 = fib2
+#      fib2 = fib_sum
+#      fib.append(fib_sum)
+#      i += 1
+#     return fib
 
-def antifib(n):
-    fib1 = 1
-    fib2 = -1
-    fib = [fib1, fib2]
-    i = 0
-    while i < n - 3:
-        fib_sum = fib1 - fib2
-        fib1 = fib2
-        fib2 = fib_sum
-        fib.append(fib_sum)
-        # fib.reverse
-        i += 1
-    # fibs = reversed(fib)
-    return fib
+# def antifib(n):
+#     fib1 = 1
+#     fib2 = -1
+#     fib = [fib1, fib2]
+#     i = 0
+#     while i < n - 3:
+#         fib_sum = fib1 - fib2
+#         fib1 = fib2
+#         fib2 = fib_sum
+#         fib.append(fib_sum)
+#         # fib.reverse
+#         i += 1
+#     # fibs = reversed(fib)
+#     return fib
 
-W = input("Введите число: ")
-W = int(W)
+# W = input("Введите число: ")
+# W = int(W)
 
-print(antifib(W) + fibonachi(W))
+# print(antifib(W) + fibonachi(W))
 
 
 
@@ -59,3 +59,20 @@ print(antifib(W) + fibonachi(W))
 # print(str_fib)
 # int_fib = [int(k) for k in str_fib]
 # print(int_fib)
+
+def negafib(n): 
+    if n in [-1,0,1]: 
+        return abs(n) 
+    elif n < 0: 
+        n = abs(n) 
+        return (-1)**(n+1)*(negafib(n-1)+negafib(n-2)) 
+    else: 
+        return negafib(n-1)+negafib(n-2) 
+
+n = int(input('Введите число n = ')) 
+list = [] 
+
+print(negafib(n))
+for i in range(-n,n+1): 
+    list.append(negafib(i)) 
+print(list)
