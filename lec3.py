015420d9a658958d8f4265217e65bac1360d3bd8
ILYAS <ailyas80@mail.ru>
2022-04-14 21:44:24 +0400

    lec3-1

--- lec3.py
+++ lec3.py
@@ -1,17 +1,5 @@
-def negafib(n): 
-    if n in [-1,0,1]: 
-        return abs(n) 
-    elif n < 0: 
-        n = abs(n) 
-        return (-1)**(n+1)*(negafib(n-1)+negafib(n-2)) 
-    else: 
-        return negafib(n-1)+negafib(n-2) 
-
-n = int(input('Введите число n = ')) 
-list = [] 
-
-print(negafib(n))
-for i in range(-n,n+1): 
-    list.append(negafib(i)) 
-print(list)
+str_fib = ['1','2']
+print(type(str_fib))
+# int_fib = [int(k) for k in str_fib]
+# print(int_fib)
 
