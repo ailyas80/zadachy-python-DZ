b0cf8c7e31b172ddf8a5c7c9394b09d61acc12d1
ILYAS <ailyas80@mail.ru>
2022-05-14 10:39:45 +0400

    lec3 repyt

--- lec3.py
+++ lec3.py
@@ -1,5 +1,5 @@
-str_fib = ['1','2']
-print(type(str_fib))
+# str_fib = ['1','2']
+# print(type(str_fib))
 # int_fib = [int(k) for k in str_fib]
 # print(int_fib)
 
@@ -15,18 +15,45 @@ data = list(enumerate(users))
 print(data)
 
 
-
-
-
-
-
-
-
 # def f(x):
 #     return x**2
 # with open('file22.txt', 'r') as file2:
 #     data2 = file2.read()
+#     print(data2)
 #     data2 = list(map(int,(data2.split())))
+#     print(data2)
 # file2.close()
 # list = [(i, f(i)) for i in data2 if i % 2 == 0]
+# print(list)
+
+
+# f = lambda q, w: q + w
+
+# def calc(op, a, b):
+#     print(op(a, b))
+#     # return op(a, b)
+
+
+# # print(f(3, 5))
+# # calc(f, 5, 5)
+
+# calc(lambda q, w: q + w, 5, 5)
+
+# list = []
+
+# for i in range(1, 21):
+#     if(i % 2 == 0):
+#         list.append(i)
+# print(list)
+
+# list = [i for i in range(1, 21) if i % 2 == 0]
+# print(list)
+
+# list = [(i, i) for i in range(1, 21) if i % 2 == 0]
+# print(list)
+
+# def f(x):
+#     return x**3
+
+# list = [(i, f(i)) for i in range(1, 21) if i % 2 == 0]
 # print(list)
