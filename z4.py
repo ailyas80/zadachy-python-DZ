048f19ccde9c439c25ff6cdfbc5fa4d53a0f75e9
ILYAS <ailyas80@mail.ru>
2022-05-16 10:13:12 +0400

    z4-4

--- z4.py
+++ z4.py
@@ -1,18 +1,18 @@
 # Показать первую цифру дробной части числа
-# N = float(input('N = '))
-# X = N * 10
-# X = X // 1
-# while X >= 10:
-#     X = round(X - 10)
-# print(X)
+N = float(input('N = '))
+X = N * 10
+X = X // 1
+while X >= 10:
+    X = round(X - 10)
+print(X)
 
-def drob(N):
-    X = N * 10
-    X = X // 1
-    while X >= 10:
-        X = round(X - 10)
-    return X
+# def drob(N):
+#     X = N * 10
+#     X = X // 1
+#     while X >= 10:
+#         X = round(X - 10)
+#     return X
 
-W = float(input('W = '))
+# W = float(input('W = '))
 
-print(drob(W))
+# print(drob(W))
