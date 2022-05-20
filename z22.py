68b7b070924cfac0e220778714e7fac81ec8c41f
ILYAS <ailyas80@mail.ru>
2022-05-20 12:22:16 +0400

    z22-22

--- z22.py
+++ z22.py
@@ -3,6 +3,6 @@ list = [1, 2, 3, 4, 5, 6, 7]
 sum = 0
 # for i in range(1,len(list),2):
 for i in range(len(list)):
-    if i% 2 !=0:
-     sum = sum + list[i]
+    if i % 2 != 0:
+        sum = sum + list[i]
 print(sum)
