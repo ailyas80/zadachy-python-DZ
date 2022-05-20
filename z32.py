c0ef011a9f5c8e174e43271b22609ffda3836051
ILYAS <ailyas80@mail.ru>
2022-05-20 20:08:31 +0400

    z32-1

--- z32.py
+++ z32.py
@@ -7,3 +7,12 @@ new = set(list)
 for i in new:
     list2.append(i)
 print(f'{list} => {list2}')
+
+# a = []  # заводим пустой список
+# n = int(input())  # считываем количество элемент в списке
+# for i in range(n):  
+#     new_element = int(input())  # считываем очередной элемент
+#     a.append(new_element)  # добавляем его в список
+#     # последние две строки можно было заменить одной:
+#     # a.append(int(input()))
+# print(a)
