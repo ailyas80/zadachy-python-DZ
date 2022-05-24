f3c126cfecfbfeeb8d2ef93a94edd0be8e861065
ILYAS <ailyas80@mail.ru>
2022-05-24 12:55:49 +0400

    z37-1

--- z37.py
+++ z37.py
@@ -1,17 +1,53 @@
-# Дан список чисел. Создать список в который попадают числа, описывающие 
-# возрастающую последовательность и содержащие максимальное количество элементов. 
+# Дан список чисел. Создать список в который попадают числа, описывающие
+# возрастающую последовательность и содержащие максимальное количество элементов.
 # Пример: [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3, 4, 6, 7]
 #         [5, 2, 3, 4, 6, 1, 7] => [2, 3, 4, 6, 7]
 #  Порядок элементов менять нельзя
-list = [1, 5, 2, 3, 4, 6, 1, 7]
-new_list = []
-max = 0
-# list = [(i, i) for i in list]
-# print(list)
-for i in list:
-    if list[i] > max:
-        max = list[i]
-        print(max)
-        new_list.append(max)
-print(new_list)
+# list = [1, 5, 2, 3, 4, 6, 1, 7]
+# new_list = []
+# max = 0
+# for i in list:
+#     if i > max:
+#         max = i
+#         new_list.append(max)
+# print(new_list)
 
+
+# list = [1, 5, 2, 3, 4, 6, 1, 7]
+# new_list = []
+# maximum = max(list)
+# for i in list:
+#     if i > maximum:
+#         # max = i
+#         new_list.append(i)
+# print(new_list)
+
+X = [1, 5, 2, 3, 4, 6, 1, 7]
+L = []
+M = []
+j = 1
+
+while j < len(X):
+    i = j
+    N = X[:]
+    while i < len(N)-1:
+        if N[i] < N[i+1]:
+            M.append(N[i])
+            i += 1
+        else:
+            N.pop(i+1)
+            print(M)
+            print(N)
+
+    j += 1
+    if len(L) < len(M):
+        L = M
+    M = []
+
+if X[-1] > L[-1]:
+    L.append(X[-1])
+
+if X[0] < L[0]:
+    L.insert(0, X[0])
+
+print(L)
